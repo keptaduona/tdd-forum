073d5de1f5ec5d2791797de8f0053831a6ee07d9
Matas Kinderis <mataskind@gmail.com>
2017-05-11 17:09:55 +0300

    mehv3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <title>Forum</title>
 
     <!-- Styles -->
-
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
     <link href="css/app.css" rel="stylesheet">
 
     <!-- Scripts -->
